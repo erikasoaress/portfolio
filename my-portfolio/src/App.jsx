0b343d56993d401db0673c864b2e42fb7b9fc0bd
Érika Soares <contacto@erikazdesign.com>
2023-05-03 16:50:12 +0100

    creation of components

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/header/Header'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <About/>
+    <Contact/>
+    <Experience/>
+    </>
+  )
+}
+
+export default App
---
+++ my-portfolio/src/components/experience/Experience.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./experience.css"
+
+
+function Experience() {
+  return (
+    <div>Experience</div>
+  )
+}
+
+export default Experience
