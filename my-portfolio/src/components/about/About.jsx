8e43b42b7bdd739ee0b95d2fb6b7ca0424d139cc
Érika Soares <contacto@erikazdesign.com>
2023-05-04 22:53:26 +0100

    about me picture

--- my-portfolio/src/components/about/About.jsx
+++ my-portfolio/src/components/about/About.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
-import "./about.css"
+import React from "react";
+import "./about.css";
+import ME2 from "../../assets/me2.png";
+import { GrVirtualMachine } from "react-icons/gr";
 
 function About() {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME2} alt="profile picture of Erika" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <GrVirtualMachine className="about__card" />
+              <h5>Experience</h5>
+            </article>
+
+            <article className="about__card">
+              <GrVirtualMachine className="about__card" />
+              <h5>Experience</h5>
+            </article>
+
+            <article className="about__card">
+              <GrVirtualMachine className="about__card" />
+              <h5>Experience</h5>
+            </article>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
