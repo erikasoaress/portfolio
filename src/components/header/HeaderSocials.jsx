98a5a6650ca395b97735f6a15f35c26b87f5d958
Érika Soares <contacto@erikazdesign.com>
2023-05-10 18:06:42 +0100

    header icons size

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -9,10 +9,10 @@ function HeaderSocials() {
         href="https://www.linkedin.com/in/%C3%A9rika-soares-gomes-496704241/?locale=en_US"
         target="_blank"
       >
-        <CiLinkedin />
+        <CiLinkedin className="header__socials-icon"/>
       </a>
       <a href="https://github.com/erikasoaress" target="_blank">
-        <BsGithub />
+        <BsGithub className="header__socials-icon"/>
       </a>
     </div>
   );
