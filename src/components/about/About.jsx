cf5fe50624563d0d314a2c821b22723ae663975a
Érika Soares <contacto@erikazdesign.com>
2023-05-12 18:35:07 +0100

    new text about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -36,13 +36,26 @@ function About() {
           <article className="about__card">
             <p>
               {" "}
-              I'm a Front-End Developer from Brasil, Rio de Janeiro based in Lisbon, has been 4 years. I have a background in jewelry design and entrepreneurship, but after many years in the industry, developing physical products, I felt the need to challenge
-              myself in a new field, using my creativity in new ways. The tech field made me feel curious and I decided to switch things up and jump in this new world. I am a creative thinker who wants to collaborate with dynamic teams in order to achieve a common goal, that will have an impact in peoples lives.
+              I'm a Front-End Developer originally from Rio de Janeiro, Brazil,
+              and I have been based in Lisbon for the past four years. With a
+              background in jewelry design and entrepreneurship, I bring a
+              strategic perspective to website design and user experience.
+              However, after many years in the industry, developing physical
+              products, I felt the need to challenge myself in a new field and
+              use my creativity to build intelligent solutions that are
+              accessible to a wide range of people, providing delightful user
+              experiences. The tech industry piqued my curiosity, and I decided
+              to pivot my career.In addition to my technical skills, I strongly
+              believe in creating inclusive environments that value and
+              prioritize diversity. I strive to bring this mindset to every
+              project I work on, as I believe it leads to more thoughtful and
+              effective solutions that can benefit all users equally. As a
+              creative thinker, I am passionate about collaborating with dynamic
+              teams to achieve a common goal that can positively impact people's
+              lives.
             </p>
           </article>
         </div>
-
-     
       </div>
     </section>
   );
