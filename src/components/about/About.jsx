adfbecea520ea2a0849b7ab32cb7defcab812ace
Érika Soares <contacto@erikazdesign.com>
2023-05-12 18:58:04 +0100

    text edition

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -36,23 +36,15 @@ function About() {
           <article className="about__card">
             <p>
               {" "}
-              I'm a Front-End Developer originally from Rio de Janeiro, Brazil,
-              and I have been based in Lisbon for the past four years. With a
-              background in jewelry design and entrepreneurship, I bring a
-              strategic perspective to website design and user experience.
-              However, after many years in the industry, developing physical
-              products, I felt the need to challenge myself in a new field and
-              use my creativity to build intelligent solutions that are
-              accessible to a wide range of people, providing delightful user
-              experiences. The tech industry piqued my curiosity, and I decided
-              to pivot my career.In addition to my technical skills, I strongly
-              believe in creating inclusive environments that value and
-              prioritize diversity. I strive to bring this mindset to every
-              project I work on, as I believe it leads to more thoughtful and
-              effective solutions that can benefit all users equally. As a
-              creative thinker, I am passionate about collaborating with dynamic
-              teams to achieve a common goal that can positively impact people's
-              lives.
+              I'm a Front-End Developer from Rio de Janeiro, Brazil, now based
+              in Lisbon. With a background in jewelry design and
+              entrepreneurship, I bring a strategic perspective to website
+              design and user experience. After years in the industry, I
+              switched to tech to build intelligent solutions that delight
+              users. I value creating inclusive environments that prioritize
+              diversity, believing this leads to more effective solutions. I'm a
+              creative thinker, passionate about collaborating with dynamic
+              teams to positively impact people's lives.
             </p>
           </article>
         </div>
